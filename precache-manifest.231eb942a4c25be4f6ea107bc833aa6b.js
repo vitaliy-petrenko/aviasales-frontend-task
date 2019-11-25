c652d4ee19a8492b441f94003d19d5d274c078e9
Vitaliy Petrenko <lpdev.vitaliy@gmail.com>
2019-11-25 02:03:53 +0200

    Updates

---
+++ precache-manifest.231eb942a4c25be4f6ea107bc833aa6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d76472d9f2882aa0860532a7aa6cd4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fefea4e183a4f4088a8f",
+    "url": "/static/css/main.50a44813.chunk.css"
+  },
+  {
+    "revision": "c401db59157ff6ce474d",
+    "url": "/static/js/2.bfff3bf3.chunk.js"
+  },
+  {
+    "revision": "fefea4e183a4f4088a8f",
+    "url": "/static/js/main.ccfbf582.chunk.js"
+  },
+  {
+    "revision": "2efd55266102d6d8daf2",
+    "url": "/static/js/runtime-main.6ef13097.js"
+  },
+  {
+    "revision": "b73f59a3543a8ad3711bc337ad929297",
+    "url": "/static/media/check.b73f59a3.svg"
+  },
+  {
+    "revision": "b3042ea45f28509ab0b8028325ae2682",
+    "url": "/static/media/earth.b3042ea4.svg"
+  },
+  {
+    "revision": "2eb1e28caa4987a0df1be0bf350c6eca",
+    "url": "/static/media/plane.2eb1e28c.svg"
+  }
+]);
