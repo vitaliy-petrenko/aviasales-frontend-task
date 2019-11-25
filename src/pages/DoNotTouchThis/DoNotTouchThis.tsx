55bc4b6939c4b051fb68dafb52618fe07b85fb0e
Vitaliy Petrenko <lpdev.vitaliy@gmail.com>
2019-11-25 04:44:38 +0200

    fix: remove log

--- src/pages/DoNotTouchThis/DoNotTouchThis.tsx
+++ src/pages/DoNotTouchThis/DoNotTouchThis.tsx
@@ -3,31 +3,27 @@ import './DoNotTouchThis.scss'
 import Button from '../../components/ui/forms/Button'
 import { Link } from 'react-router-dom'
 
-const WTF: React.FC = () => {
-  console.log('render')
-
-  return (
-    <div className='do-not-touch-this'>
-      <div className='do-not-touch__d'></div>
-      <div className='do-not-touch__o'></div>
-      <div className='do-not-touch__n'></div>
-      <div className='do-not-touch__o'></div>
-      <div className='do-not-touch__t'></div>
-      <div className='do-not-touch__t'></div>
-      <div className='do-not-touch__o'></div>
-      <div className='do-not-touch__u'>
-        <Link to='/' className='yo-btn-wrap'>
-          <Button size='lg' div type='secondary'>Уйти</Button>
-        </Link>
-      </div>
-      <div className='do-not-touch__c'></div>
-      <div className='do-not-touch__h'></div>
-      <div className='do-not-touch__t'></div>
-      <div className='do-not-touch__h'></div>
-      <div className='do-not-touch__i'></div>
-      <div className='do-not-touch__s'></div>
+const WTF: React.FC = () => (
+  <div className='do-not-touch-this'>
+    <div className='do-not-touch__d'></div>
+    <div className='do-not-touch__o'></div>
+    <div className='do-not-touch__n'></div>
+    <div className='do-not-touch__o'></div>
+    <div className='do-not-touch__t'></div>
+    <div className='do-not-touch__t'></div>
+    <div className='do-not-touch__o'></div>
+    <div className='do-not-touch__u'>
+      <Link to='/' className='yo-btn-wrap'>
+        <Button size='lg' div type='secondary'>Уйти</Button>
+      </Link>
     </div>
-  )
-}
+    <div className='do-not-touch__c'></div>
+    <div className='do-not-touch__h'></div>
+    <div className='do-not-touch__t'></div>
+    <div className='do-not-touch__h'></div>
+    <div className='do-not-touch__i'></div>
+    <div className='do-not-touch__s'></div>
+  </div>
+)
 
 export default WTF
