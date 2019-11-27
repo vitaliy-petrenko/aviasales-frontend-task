f2242e0dbc59d2ec3130fcc719799e02e738fbda
Vitaliy Petrenko <lpdev.vitaliy@gmail.com>
2019-11-27 11:18:57 +0200

    refactor: Checkbox component optimization 2

--- src/components/ui/forms/Checkbox/Checkbox.tsx
+++ src/components/ui/forms/Checkbox/Checkbox.tsx
@@ -2,16 +2,10 @@ import React from 'react'
 import { ReactComponent as ICheck } from '../../../../assets/icons/check.svg'
 import './Checkbox.scss'
 
-interface IProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
-  type?: string
-
-  onChange(value: React.ChangeEvent<HTMLInputElement>): void
-}
-
-const Checkbox: React.FC<IProps> = ({ children, onChange, type = 'checkbox', ...rest }) => {
+const Checkbox: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ children, onChange, type = 'checkbox', checked }) => {
   return (
     <label className='checkbox'>
-      <input type={type} onChange={onChange} {...rest} />
+      <input type={type} onChange={onChange} checked={checked}/>
       <span className='checkbox__icon'><ICheck/></span>
 
       <span className='checkbox__label'>
