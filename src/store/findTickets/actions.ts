c604fd14993b26177a41c45046e1b12525cecf82
Vitaliy Petrenko <lpdev.vitaliy@gmail.com>
2019-12-11 00:44:37 +0200

    fix: action type

--- src/store/findTickets/actions.ts
+++ src/store/findTickets/actions.ts
@@ -1,5 +1,5 @@
 import { createAction } from '@reduxjs/toolkit'
 
-export const fetchTicketsRequest = createAction<never>(`TICKETS_FETCH_REQUESTED`)
+export const fetchTicketsRequest = createAction(`TICKETS_FETCH_REQUESTED`)
 
 export const clearFindTickets = createAction(`TICKETS_CLEAR`)
